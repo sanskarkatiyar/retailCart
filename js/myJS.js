f4f3578efe7f2b6573f1c2f75dc43626a2f61a79
Sanskar Katiyar <sanskarkatiyar@gmail.com>
2017-03-27 03:48:08 +0530

    ajax_cart
    ajax script for cart operations and bug fixes

--- js/myJS.js
+++ js/myJS.js
@@ -4,14 +4,94 @@
 //AJAX functions to edit the product listings in the shopping cart
 //This would make it easier for the user to edit products then and there.
 
-function deleteProduct(productID){
+
+function addToCart(productID, units) {
+    if (productID == "" && units !=0) {
+        alert("Invalid Request!");
+    } else {
+        
+        xmlhttp = new XMLHttpRequest();
+        var url = "cart.php";
+        
+        xmlhttp.open('POST', url, true);
+        xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                
+                newProductAdded = this.responseText; //returns completely formatted product row
+                
+                var productList = document.getElementById("cartProductList");
+                productList.innerHTML = productList.innerHTML + newProductAdded;
+                //SHOW A COLLAPSING NOTIFICATION: ITEM ADDED TO CART
+            }
+        }
+        
+        xmlhttp.send("action=addItem&productID=" + productID + "&units=" + units);
+    }
     
 }
 
-function incrementProductCount(productID){
-    
+function deleteProduct(productID) {
+    if (productID == "") {
+        alert("Invalid Request!");
+    } else {
+        xmlhttp = new XMLHttpRequest();
+        var url = "cart.php";
+        
+        xmlhttp.open('POST', url, true);
+        xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById(productID).style.display = "None";
+                //document.getElementById(productID).outerHTML = "";
+            }
+        }
+        
+        xmlhttp.send("action=removeItem&productID=" + productID);
+    }
 }
 
-function decrementProductCount(productID){
-    
+
+function incrementProductCount(productID, count) {
+    if (productID == "" || count==10) { //maxiumum count 10 per product
+        alert("Invalid Request! Product count Limit reached.");
+    } else {
+        xmlhttp = new XMLHttpRequest();
+        var url = "cart.php";
+        
+        xmlhttp.open('POST', url, true);
+        xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                var c = document.getElementById(productID + '_count');
+                c.innerHTML = parseInt(c.innerHTML) + 1;
+            }
+        }
+        
+        xmlhttp.send("action=increaseItemCount&productID=" + productID);
+    }    
+}
+
+function decrementProductCount(productID) {
+    if (productID == "" || count==1) { //miniumum count 1 per product
+        alert("Invalid Request! Product count Limit reached.");
+    } else {
+        xmlhttp = new XMLHttpRequest();
+        var url = "cart.php";
+        
+        xmlhttp.open('POST', url, true);
+        xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                var c = document.getElementById(productID + '_count');
+                c.innerHTML = parseInt(c.innerHTML) - 1;
+            }
+        }
+        
+        xmlhttp.send("action=decreaseItemCount&productID=" + productID);
+    }        
 }
