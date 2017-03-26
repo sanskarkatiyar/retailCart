b79d4ab6500573d5a856a651ed9266b4083cd318
Sanskar Katiyar <sanskarkatiyar@gmail.com>
2017-03-26 23:45:29 +0530

    cart - front end
    finally done with the front-end design of the cart. changed from the
    bootstrap grid to a table.

---
+++ js/myJS.js
@@ -0,0 +1,17 @@
+//myJS.js
+//Sanskar Katiyar
+
+//AJAX functions to edit the product listings in the shopping cart
+//This would make it easier for the user to edit products then and there.
+
+function deleteProduct(productID){
+    
+}
+
+function incrementProductCount(productID){
+    
+}
+
+function decrementProductCount(productID){
+    
+}
